150ce4224e31dec4603fb6609964bca915b811c6
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-14 17:22:34 +0530

    terrafrom

---
+++ data-sourses/data.tf
@@ -0,0 +1,13 @@
+
+# devops practice RHEL AMI Owner account ID : 973714476881
+
+data "aws_ami" "vs-devops" {
+  most_recent = true
+  owners      = ["973714476881"]
+
+  filter {
+    name   = "name"
+    values = ["RHEL-9-DevOps-Practice"]
+  }
+
+}
---
+++ data-sourses/output.tf
@@ -0,0 +1,4 @@
+#this file is only fot see the info not used
+output "ami_id" {
+  value = data.aws_ami.vs-devops.id
+}
