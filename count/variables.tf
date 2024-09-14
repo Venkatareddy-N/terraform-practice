ce888659c253b3c59222ca8e377cf6aecbd0fe8e
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-14 13:24:13 +0530

    terrafrom

---
+++ count/variables.tf
@@ -0,0 +1,54 @@
+variable "instance_names" {
+    type = list(string)
+    default = ["mysql", "backend"]
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "ami_id" {
+  type    = string
+  default = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "from_port" {
+  type    = number
+  default = 22
+}
+
+variable "to_port" {
+  type    = number
+  default = 22
+}
+
+variable "protocol" {
+  type    = string
+  default = "tcp"
+}
+
+variable "ingress_cidr" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "tags" {
+  type = map(any)
+  default = {
+    Name        = "DevOps-Server"
+    Environment = "Dev"
+    Component   = "Backend"
+    Terraform   = "True"
+    Project     = "Expense"
+  }
+}
