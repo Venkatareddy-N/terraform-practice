323e288b7eeba8723cb3b37cf882be3ffac52887
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-14 21:44:14 +0530

    expense-project

---
+++ expense-project/data.tf
@@ -0,0 +1,13 @@
+
+# devops practice RHEL AMI Owner account ID : 973714476881
+
+data "aws_ami" "devops_ami" {
+  most_recent = true
+  owners      = ["973714476881"]
+
+  filter {
+    name   = "name"
+    values = ["RHEL-9-DevOps-Practice"]
+  }
+
+}
---
+++ expense-project/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "expense" {
+  ami                    = data.aws_ami.devops_ami.id
+  instance_type          = var.instance_names[count.index] == "mysql" ? "t3.small" : "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  count                  = length(var.instance_names)
+
+  tags = {
+    Name = var.instance_names[count.index]
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name = "allow-ssh"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Allow-SSH"
+  }
+
+}
