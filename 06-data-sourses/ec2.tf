af066400a16bc18eee6bfc76ebca2658f9df6365
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-18 06:48:54 +0530

    removed extra SG

--- 06-data-sourses/ec2.tf
+++ 06-data-sourses/ec2.tf
@@ -2,60 +2,32 @@
 resource "aws_instance" "terraform_server" {
   ami                    = data.aws_ami.vs-devops.id
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_80.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "DevOps-Master"
   }
 }
 
-
 resource "aws_security_group" "allow_ssh" {
   name = "allow-ssh"
 
   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     Name = "Allow-SSH"
-  }
-
-}
-
-resource "aws_security_group" "allow_80" {
-  name = "allow-80"
-
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow-80"
   }
 
 }
