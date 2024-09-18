6044a7741eabf947317185a3d3a0be80d50c2423
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-18 20:40:47 +0530

    terraform multi vars

--- 13-terraform-multiple-env-tfvars/tfvars/ec2.tf
+++ 13-terraform-multiple-env-tfvars/tfvars/ec2.tf
@@ -11,3 +11,31 @@ resource "aws_instance" "expense" {
   )
 }
 
+resource "aws_security_group" "allow-ssh" {
+  name = "allow-ssh_${var.environment}"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.common_tags,
+    var.tags,
+    {
+      Name = "allow-ssh_${var.environment}"
+    }
+  )
+
+}
